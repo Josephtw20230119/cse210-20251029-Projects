1e197ec1abb060aee86a7903f4409a3fb8918879
Tsung Ying Wu <123164547+Josephtw20230119@users.noreply.github.com>
2025-11-07 19:43:56 +0800

    C# Programming Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // input
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        // output
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {first_name}, {first_name} {last_name}.");
     }
 }
+/*
+What is your first name? Scott
+What is your last name? Burton
+
+Your name is Burton, Scott Burton.
+*/
