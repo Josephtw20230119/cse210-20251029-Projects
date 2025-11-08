3396ff53182c63b5ce386b17a89e701559d43cb3
Tsung Ying Wu <123164547+Josephtw20230119@users.noreply.github.com>
2025-11-08 11:48:05 +0800

    C# Programming Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Globalization;
 
-class Program
+
+
+class Program(string[] args)
 {
+    static string userName = "";
+    //int intNumber = 0;
+    //string strNumber = "";
+    static int userNumber = 0;
+    static int squareNumber = 0;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
+        SquareNumber();
+        DisplayResult();
+
+
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static void PromptUserName()
+    {
+        
+        Console.Write("Please enter you name : ");
+        userName = Console.ReadLine();
+    }
+    static void PromptUserNumber()
+    {
+        string strNumber = "";
+    
+        bool success;
+        Console.Write("Please enter you favorite number : ");
+        strNumber = Console.ReadLine();
+
+        success = int.TryParse(strNumber, out userNumber);
+        if (!success)
+        {
+            Console.WriteLine("Please Enter a valid Number!");
+        }
+    }
+    static void SquareNumber()
+    {
+        squareNumber = userNumber * userNumber;
+    }
+    static void DisplayResult()
+    {
+        Console.WriteLine(userName);
+       Console.WriteLine($"{userName}, the square of you number is {squareNumber}");
+    }
+
+
 }
+
+/*
+Welcome to the program!
+Please enter your name: Brother Burton
+Please enter your favorite number: 42
+Brother Burton, the square of your number is 1764
+*/
