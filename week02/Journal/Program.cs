739ce8cc412c034ab54057160643ab1e50eb83dc
Tsung Ying Wu <123164547+Josephtw20230119@users.noreply.github.com>
2025-11-16 02:55:38 +0800

    Merge Program.cs

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,8 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        //yryryrhhiiConsole.WriteLine("Hello World! This is the Journal Project.");
+        // I have been creative and did more than what was asked. 
+        // I made the UI better and added more classes (App, UI, Prompt, Entry, Journal, FileManager) to make the code easier to use and understand.
         App app = new App();
         app.Execute();
+        
     }
 }
