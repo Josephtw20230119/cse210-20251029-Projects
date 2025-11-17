54f71d7b3a73eb648b98fca6a3943024314a8417
Tsung Ying Wu <123164547+Josephtw20230119@users.noreply.github.com>
2025-11-17 18:31:43 +0800

    create Fraction Class

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        //Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(5);
+        
+        Console.Clear();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+        first.Blank_Line();
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+        second.Blank_Line();
+        Fraction third1 = new Fraction(3, 4);
+        Console.WriteLine(third1.GetFractionString());
+        Console.WriteLine(third1.GetDecimalValue());
+        third1.Blank_Line();
+        Fraction third2 = new Fraction(1, 3);
+        Console.WriteLine(third2.GetFractionString());
+        Console.WriteLine(third2.GetDecimalValue());
+        
+    
     }
 }
