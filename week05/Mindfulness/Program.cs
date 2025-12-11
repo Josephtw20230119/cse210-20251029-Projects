20fea050fc49f03f63b4b41e404355c6f690418b
Tsung Ying Wu <123164547+Josephtw20230119@users.noreply.github.com>
2025-12-11 17:20:45 +0800

    Update Program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //using colors, animations, clear formatting, and a well-organized class structure for a better user experience.
         //Console.WriteLine("Hello World! This is the Mindfulness Project.");
         App app = new App();
         app.Execute();
