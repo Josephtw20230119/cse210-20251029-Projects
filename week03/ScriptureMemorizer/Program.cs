14cdb4b99aea04dbc22f125411d8dd1877c99d7b
Tsung Ying Wu <123164547+Josephtw20230119@users.noreply.github.com>
2025-12-12 04:10:03 +0800

    Update Program.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // improve the user interface and redesign App.cs and UI.cs to make the code more flexible and maintainable.
         //Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
         App app = new App();
 
